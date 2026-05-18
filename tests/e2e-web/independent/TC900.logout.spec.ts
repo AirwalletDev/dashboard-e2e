5d3a5d9216c3dd818d151babfdbaac8a97bc9271
zinajda <zinajda@zinajdas-MacBook-Pro.local>
2026-05-18 09:53:32 +0200

    fix configs

--- tests/e2e-web/independent/TC900.logout.spec.ts
+++ tests/e2e-web/independent/TC900.logout.spec.ts
@@ -11,6 +11,8 @@ test('User can successfully log out from Airwallet dashboard', async ({page}) =>
         email: process.env.TEST_USER_EMAIL ?? '',
         password: process.env.TEST_USER_PASSWORD ?? '',
     };
+    console.log(process.env.TEST_USER_EMAIL);
+    console.log(process.env.TEST_USER_PASSWORD);
     await loginPage.givenUserIsOnSignInPage();
     await loginPage.whenUserFillsInSignInForm(staticUser.email, staticUser.password);
     await loginPage.whenTheUserClicksSignInButton();
