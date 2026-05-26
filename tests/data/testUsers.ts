4ec4a072c56cbd80ab9a931caf9756dd8e7a6194
Zinajda Sarac <zinajda@airwallet.net>
2026-05-26 15:27:59 +0200

    test users not in env var

--- tests/data/testUsers.ts
+++ tests/data/testUsers.ts
@@ -1,14 +1,6 @@
-function requireEnv(name: string): string {
-    const value = process.env[name];
-    if (!value) {
-        throw new Error(`${name} environment variable is missing`);
-    }
-    return value;
-}
-
 export const testUsers = {
     logoutUser: {
-        email: requireEnv('TEST_USER_EMAIL'),
-        password: requireEnv('TEST_USER_PASSWORD'),
+        email: 'zinajda+44@airwallet.net',
+        password: 'Airwallet2026!',
     },
 };
